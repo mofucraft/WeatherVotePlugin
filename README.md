fe02e5ef00cff43508821adf4e1bc255cec2099f
いね <ine0056@gmail.com>
2021-05-23 11:20:36 +0900

    version-1.0

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # Weather vote plugin
-Weather vote plugin は プレイヤーの投票により、晴れにするプラグインです。
+Weather vote plugin は プレイヤーの投票により、晴れにするBukkitプラグインです。
 
 ・反対者が一人いた場合にはそのままの気候
 
-# TODO
-- [x]  nullの解析 (Issues)
+・ワールド「world」のみ晴れに変更
 
-- [x]  反対募集期間中に新たに投票を開始させない
+・投票受付中には投票を開始させることはできません。（二重投票開始をしない）
+
+・5分おきに投票ができるように
+
+# Commands
+
+| コマンド | エイリアス | 説明 | 権限 |
+| --- | :---: | :---: | --- |
+|/mvote vote| sun,day |投票を開始する|default|
+|/mvote oppose| |投票に反対する|default|
+|/mvote voteinfo| |現在の投票の状態を見る|default|
--- src/main/java/ine0056/github/com/weathervoteplugin/Weathervoteplugin.java
+++ src/main/java/ine0056/github/com/weathervoteplugin/Weathervoteplugin.java
@@ -22,9 +22,11 @@ public final class Weathervoteplugin extends JavaPlugin implements CommandExecut
     public void onDisable() {
         // Plugin shutdown logic
     }
-    
+
     private static int weatherchangetask;
     private static String votecomamnd;
+    private static String fivemintask;
+    private static int fivemintasktask;
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
@@ -34,59 +36,57 @@ public final class Weathervoteplugin extends JavaPlugin implements CommandExecut
             if (command.getName().equalsIgnoreCase("mvote")) {
                 if (args.length != 0) {
 
-                    if (args[0].equalsIgnoreCase("day")||args[0].equalsIgnoreCase("sun")||args[0].equalsIgnoreCase("vote")) {
-                        String pworld = player.getWorld().getName();
-                        if ((votecomamnd == "nonvote") || (votecomamnd == null)) {
-                            if (pworld == "world") {
-                                Bukkit.broadcastMessage(ChatColor.AQUA + "1分後に天気を晴れにします。中断する場合は" + ChatColor.RED + "/mvote oppose " + ChatColor.AQUA + "と入力してください。");
+                    if (args[0].equalsIgnoreCase("day") || args[0].equalsIgnoreCase("sun") || args[0].equalsIgnoreCase("vote")) {
+                        if ((fivemintask == "ok") || (fivemintask == null)) {
+                            if ((votecomamnd == "nonvote") || (votecomamnd == null)) {
+                                Bukkit.broadcastMessage(ChatColor.AQUA + "1分後に" + ChatColor.RED + "建築ワールド" + ChatColor.AQUA + "の天気を晴れにします。中断する場合は" + ChatColor.RED + "/mvote oppose " + ChatColor.AQUA + "と入力してください。");
                                 votecomamnd = "voted";
-                                World world = player.getWorld();
-                        /*  1分後に以下のプログラムを実行
-                            Bukkit.broadcastMessage("天候を変更しました");
-                            String command = "weather clear";
-                         */
+                                fivemintask = "no";
+                                World world = Bukkit.getServer().getWorld("world");
+                                /*  1分後に以下のプログラムを実行
+                                    Bukkit.broadcastMessage("天候を変更しました");
+                                    world のみ天候を晴れに変更
+                                */
                                 weatherchangetask = Bukkit.getScheduler().runTaskLater(this, () -> { //指定したTick後に処理を実行する
                                     world.setStorm(false);
                                     world.setThundering(false);
                                     Bukkit.broadcastMessage(ChatColor.AQUA + "天候を変更しました");
                                     votecomamnd = "nonvote";
                                 }, 1200L).getTaskId();
-                                return true;
-                            } else {
-                                sender.sendMessage(ChatColor.RED + "このワールドでは天候を変更できません！");
-                                sender.sendMessage("Player world is " + pworld);
-                                return true;
-                            }
-                        } else if ((votecomamnd == "voted")) {
-                            if (pworld == "world" ) {
-                                sender.sendMessage(ChatColor.RED + "既に投票が開始済みです");
-                                return true;
-                            } else {
-                                sender.sendMessage(ChatColor.RED + "すでに投票が開始済みかつこのワールドでは天候を変更できません！");
+                                fivemintasktask = Bukkit.getScheduler().runTaskLater(this, () -> {
+                                    fivemintask = "ok";
+                                }, 6000L).getTaskId();
                                 return true;
                             }
+                        } else if ((fivemintask == "no") && (votecomamnd == "voted")) {
+                            sender.sendMessage(ChatColor.RED + "既に投票が開始済みです");
+                            return true;
+                        } else if ((fivemintask == "no") && (votecomamnd == "nonvote")) {
+                            sender.sendMessage(ChatColor.RED + "前回の投票から5分経っていないため、投票を開始できません");
+                            return true;
                         }
-                    }
-
-                    else if (args[0].equalsIgnoreCase("oppose")) {
+                    } else if (args[0].equalsIgnoreCase("oppose")) {
                         if (votecomamnd == "voted") {
                             Bukkit.broadcastMessage(ChatColor.YELLOW + "反対があったため、天候の変更を中断しました");
                             //  タイマーを止める
                             Bukkit.getScheduler().cancelTask(weatherchangetask); //予約したタスクをキャンセルする
                             votecomamnd = "nonvote";
                         } else {
-                          sender.sendMessage(ChatColor.RED + "投票が開始されていないため、このコマンドを実行できません。");
+                            sender.sendMessage(ChatColor.RED + "投票が開始されていないため、このコマンドを実行できません。");
                         }
                         return true;
-                    }
-
-                    else if (args[0].equalsIgnoreCase("voteinfo")) {
-                        if (votecomamnd == "voted") {
-                            sender.sendMessage(ChatColor.DARK_AQUA + "現在投票は有効です");
+                    } else if (args[0].equalsIgnoreCase("voteinfo")) {
+                        if ((votecomamnd == "nonvote") && (fivemintask == "ok")) {
+                            sender.sendMessage(ChatColor.DARK_AQUA + "現在投票可能です");
                             return true;
-                        }
-                        else if (votecomamnd == "nonvote") {
-                            sender.sendMessage(ChatColor.DARK_AQUA + "現在投票は無効です");
+                        } else if ((votecomamnd == "voted") && (fivemintask == "ok")) {
+                            sender.sendMessage(ChatColor.DARK_AQUA + "例外：これが表示されたらおかしいよ（運営に連絡してください）");
+                            return true;
+                        } else if ((votecomamnd == "nonvote") && (fivemintask == "no")) {
+                            sender.sendMessage(ChatColor.DARK_AQUA + "現在投票を開始できません");
+                            return true;
+                        } else if ((votecomamnd == "voted") && (fivemintask == "no")) {
+                            sender.sendMessage(ChatColor.DARK_AQUA + "現在投票中です");
                             return true;
                         }
                     }
